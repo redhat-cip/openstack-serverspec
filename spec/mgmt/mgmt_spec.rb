c9fcc76d18b4081fd1c103a336583c2ae099762d
Frederic Lepied <frederic.lepied@enovance.com>
2014-01-29 08:05:21 +0100

    spec/mgmt/mgmt_spec.rb: fix rabbitmq.config check

--- spec/mgmt/mgmt_spec.rb
+++ spec/mgmt/mgmt_spec.rb
@@ -122,5 +122,5 @@ describe file('/etc/rabbitmq/rabbitmq.config') do
   it { should be_mode 644 }
   it { should be_owned_by 'root' }
 
-  its(:content) { should match /\{rabbit, \[\{cluster_nodes, \['rabbit@#{property[:hostname]}'\]\}\]\}/ }
+  its(:content) { should match /\{rabbit, \[\{cluster_nodes, \[.*'rabbit@#{property[:hostname]}'.*\]\}\]\}/ }
 end
