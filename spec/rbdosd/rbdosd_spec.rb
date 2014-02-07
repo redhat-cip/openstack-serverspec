eb88d5e88255eecd5df056d17427b6c65450dd16
Frederic Lepied <frederic.lepied@enovance.com>
2014-02-07 16:26:14 +0100

    rbdosd_spec.rb: workaround non fixed names

--- spec/rbdosd/rbdosd_spec.rb
+++ spec/rbdosd/rbdosd_spec.rb
@@ -31,22 +31,22 @@ describe port(6804) do
   it { should be_listening.with('tcp') }
 end
 
-describe command('ceph --admin-daemon /var/run/ceph/ceph-osd.0.asok dump_historic_ops') do
+describe command('ceph --admin-daemon $(ls /var/run/ceph/ceph-osd.*.asok|head -1) dump_historic_ops') do
   it { should return_exit_status 0 }
 end
 
 describe command('mount') do
-  its(:stdout) { should match /\/var\/lib\/ceph\/osd\/ceph-0 type xfs/ }
+  its(:stdout) { should match /\/var\/lib\/ceph\/osd\/ceph-. type xfs/ }
 end
 
-describe file('/var/lib/ceph/osd/ceph-0/fsid') do
-  it { should be_file }
+describe command('test -s "$(ls /var/lib/ceph/osd/ceph-*/fsid|head -1)"') do
+  it { should return_exit_status 0 }
 end
 
-describe file('/var/lib/ceph/osd/ceph-0/ready') do
-  it { should be_file }
+describe command('test -s "$(ls /var/lib/ceph/osd/ceph-*/ready|head -1)"') do
+  it { should return_exit_status 0 }
 end
 
-describe file('/var/lib/ceph/osd/ceph-0/keyring') do
-  it { should be_file }
+describe command('test -s "$(ls /var/lib/ceph/osd/ceph-*/keyring|head -1)"') do
+  it { should return_exit_status 0 }
 end
