a03ff3c55b560afa6ee2fbbce50cb34d3abbbca8
Leseb <sebastien.han@enovance.com>
2014-02-08 18:54:38 +0100

    Add glance-api and glance-registry port
    Add glance-api and glance-registry port to the check.

--- spec/image/image_spec.rb
+++ spec/image/image_spec.rb
@@ -16,3 +16,10 @@ describe service('glance-registry') do
     it {should be_running }
 end
 
+describe port(9292) do
+  it { should be_listening.with('tcp') }
+end
+
+describe port(9191) do
+  it { should be_listening.with('tcp') }
+end
