06f85fe4cb5564106fd1eebc611f99fe26674a49
Emilien Macchi <emilien.macchi@enovance.com>
2014-02-26 14:41:08 +0100

    telemetry: fix mongodb check
    use ip address instead of loopback.
    
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- spec/telemetry/telemetry_spec.rb
+++ spec/telemetry/telemetry_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 #
 # Ceilometer
 #
-describe command('echo "db.system.indexes.find()" | mongo 127.0.0.1:27017/ceilometer') do
+describe command("echo \"db.system.indexes.find()\" | mongo \"#{property[:server_ip]}\":27017/ceilometer") do
   its(:stdout) { should match /"ns" : "ceilometer.resource"/ }
 end
