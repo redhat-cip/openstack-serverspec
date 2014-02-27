89ce73168255424852c10e872ab16a11e7f564d2
Emilien Macchi <emilien.macchi@enovance.com>
2014-02-27 17:04:08 +0100

    delete telemetry folder
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- spec/telemetry/telemetry_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-#
-# Ceilometer
-#
-describe command("echo \"db.system.indexes.find()\" | mongo \"#{property[:server_ip]}\":27017/ceilometer") do
-  its(:stdout) { should match /"ns" : "ceilometer.resource"/ }
-end
