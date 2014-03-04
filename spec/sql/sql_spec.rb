4682a3101da16b2c37a156b4d3324d8396f132e8
Nicolas Hicher <nicolas.hicher@enovance.com>
2014-03-04 11:30:52 -0500

    split mgmt in 3 directories: sql, nosql, messaging

---
+++ spec/sql/sql_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+#################################
+#
+#         NON-OPENSTACK
+#
+#################################
+
+#
+# MySQL : Ensure the MySQL installation went smoothly
+# 
+# To ensure installation is correct the following
+# conditions needs to be met :
+#
+# * package 'mariadb-galera-server' needs to be installed
+# * service 'mysql' needs to be running and started at boot
+# * service has to be listening on port '3306'
+# * ensure 'sys_maint' user is installed (IS THIS CORRECT?)
+# * if the node tested is the galera master, the '--new-cluster'
+#   param needs to be one of 'mysqld' argument
+#
+
+# TODO: check user presence command.return_exit_status
+
+# On the Galera master
+# describe process('mysql') do
+#   its(:args) { shoudl match /--new-cluster/ }
+# end
+
+
+describe package('mariadb-galera-server') do
+  it { should be_installed }
+end
+
+describe service('mysql') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(3306) do
+  it { should be_listening.with('tcp') }
+end
+
+describe port(4567) do
+  it { should be_listening.with('tcp') }
+end
