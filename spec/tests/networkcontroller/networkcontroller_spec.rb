5ac5110754d8dd07326b6cbd1c6bd76d23676674
Sylvain Baubeau <sylvain.baubeau@enovance.com>
2014-03-06 14:37:26 +0100

    Add Neutron server checks

--- spec/tests/networkcontroller/networkcontroller_spec.rb
+++ spec/tests/networkcontroller/networkcontroller_spec.rb
@@ -1,1 +1,29 @@
 require 'spec_helper'
+
+describe package('neutron-server') do
+  it { should be_installed }
+end
+
+describe service('neutron-server') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(9696) do
+  it { should be_listening.with('tcp') }
+end
+
+describe file('/etc/neutron/plugins/ml2/ml2_conf.ini') do
+  its(:content) { should match /^tunnel_types ?= ?gre$/ }
+  its(:content) { should match /^type_drivers ?= ?gre,vlan$/ }
+  its(:content) { should match /^network_vlan_ranges = ?physnet1:1000:2999$/ }
+  its(:content) { should match /^tunnel_id_ranges ?= ?1:10000$/ }
+  its(:content) { should match /^mechanism_drivers ?= ?openvswitch,l2population$/ }
+  its(:content) { should match /^l2_population ?= ?True$/ }
+  its(:content) { should match /^polling_interval ?= ?2$/ }
+  its(:content) { should match /^local_ip ?= ?#{property[:server_ip]}$/ }
+  its(:content) { should match /^enable_tunneling ?= ?True$/ }
+  its(:content) { should match /^integration_bridge ?= ?br-int$/ }
+  its(:content) { should match /^bridge_mappings ?= ?physnet1:br-eth1$/ }
+  its(:content) { should match /^firewall_driver ?= ?neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver$/ }
+end
