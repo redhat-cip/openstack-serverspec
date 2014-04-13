e1ceb5a1595181bcc77d3d0675c615a67e334f98
Emilien Macchi <emilien.macchi@enovance.com>
2014-04-13 18:54:55 +0200

    netbase: ensure GRE is enabled also on RHEL

--- spec/tests/netbase/netbase_spec.rb
+++ spec/tests/netbase/netbase_spec.rb
@@ -10,12 +10,8 @@ describe kernel_module('openvswitch') do
   it { should be_loaded }
 end
 
-describe kernel_module('gre') do
-  it { should be_loaded }
-end
-
-describe kernel_module('vxlan') do
-  it { should be_loaded }
+describe command('lsmod | grep gre') do
+    it { should return_exit_status 0 }
 end
 
 describe command('ovs-vsctl show | grep br-int') do
