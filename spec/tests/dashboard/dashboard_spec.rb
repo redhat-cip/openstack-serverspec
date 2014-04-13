f0affa5d1bdc31d94fbc92e9278bc4d55c48deaa
Emilien Macchi <emilien.macchi@enovance.com>
2014-04-13 18:55:56 +0200

    dashboard: only test curl
    Delete the log file, since we use syslog

--- spec/tests/dashboard/dashboard_spec.rb
+++ spec/tests/dashboard/dashboard_spec.rb
@@ -4,10 +4,6 @@ require 'spec_helper'
 # cloud::dashboard
 #
 
-describe file('/var/log/horizon/horizon.log') do
-  it { should be_file }
-end
-
 describe command("curl #{property[:server_ip]} | grep 301") do
   it { should return_exit_status 0 }
 end
