6c38bd6e927a111ab5a288404858be817bf9e214
Emilien Macchi <emilien.macchi@enovance.com>
2014-05-06 19:34:19 +0200

    nosql: test only 27017
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- spec/tests/nosql/nosql_spec.rb
+++ spec/tests/nosql/nosql_spec.rb
@@ -11,13 +11,9 @@ require 'spec_helper'
 # To ensure installation is correct the following
 # conditions needs to be met :
 #
-# * service has to be listening on port '27017' and '28017'
+# * service has to be listening on port '27017'
 #
 
 describe port(27017) do
-  it { should be_listening.with('tcp') }
-end
-
-describe port(28017) do
   it { should be_listening.with('tcp') }
 end
