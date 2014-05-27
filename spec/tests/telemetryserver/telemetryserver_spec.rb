82743024a37a52f77aa79152af8d6bac45aa30b3
Emilien Macchi <emilien.macchi@enovance.com>
2014-05-27 20:06:53 +0200

    tests: update ceilometer tests
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- spec/tests/telemetryserver/telemetryserver_spec.rb
+++ spec/tests/telemetryserver/telemetryserver_spec.rb
@@ -8,6 +8,10 @@ describe port(8777) do
   it { should be_listening.with('tcp') }
 end
 
+describe port(27017) do
+  it { should be_listening.with('tcp') }
+end
+
 describe user('ceilometer') do
   it { should exist }
   it { should belong_to_group 'ceilometer' }
@@ -33,11 +37,4 @@ describe file('/etc/ceilometer/ceilometer.conf') do
   its(:content) { should match /^notification_topics=notifications$/ }
   its(:content) { should match /^use_syslog=True$/ }
   its(:content) { should match /^syslog_log_facility=LOG_LOCAL0$/ }
-end
-
-describe command("mongo \"#{property[:server_ip]}\":27017/ceilometer " \
-                 "--eval \"assert(db.isMaster().ismaster " \
-                 "? db.system.indexes.find({ ns: 'ceilometer.resource' }).length() == 2 " \
-                 ": true)\"") do
-  it { should return_exit_status 0 }
 end
