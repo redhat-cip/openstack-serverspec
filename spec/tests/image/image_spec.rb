085d869f9cf27e615c74871dc2a9b3763e6b5b00
Emilien Macchi <emilien.macchi@enovance.com>
2014-06-04 16:49:20 +0200

    image: fix regex to catch glance config with rbd

--- spec/tests/image/image_spec.rb
+++ spec/tests/image/image_spec.rb
@@ -17,10 +17,10 @@ describe package('python-ceph') do
 end
 
 describe file('/etc/glance/glance-api.conf') do
-  its(:content) { should match /^default_store = rbd$/ }
-  its(:content) { should match /^rbd_store_pool = images$/ }
-  its(:content) { should match /^rbd_store_user = glance$/ }
-  its(:content) { should match /^rbd_store_ceph_conf = \/etc\/ceph\/ceph.conf$/ }
+  its(:content) { should match /^default_store ?= ?rbd$/ }
+  its(:content) { should match /^rbd_store_pool ?= ?images$/ }
+  its(:content) { should match /^rbd_store_user ?= ?glance$/ }
+  its(:content) { should match /^rbd_store_ceph_conf ?= ?\/etc\/ceph\/ceph.conf$/ }
 end
 
 describe file('/etc/ceph/ceph.client.glance.keyring') do
