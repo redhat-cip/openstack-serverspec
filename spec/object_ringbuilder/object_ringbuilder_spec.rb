55a5385f4e83333342b01fadeeb1dd92db35dbeb
Frederic Lepied <frederic.lepied@enovance.com>
2014-06-09 16:27:25 +0200

    spec/object_ringbuilder/object_ringbuilder_spec.rb: fix typo

--- spec/object_ringbuilder/object_ringbuilder_spec.rb
+++ spec/object_ringbuilder/object_ringbuilder_spec.rb
@@ -1,3 +1,3 @@
 require_relative '../tests/swiftbase/swiftbase_spec.rb'
-require_relative '../tests/swiftringbuilder/swiftrinbguilder_spec.rb'
+require_relative '../tests/swiftringbuilder/swiftringbuilder_spec.rb'
 
