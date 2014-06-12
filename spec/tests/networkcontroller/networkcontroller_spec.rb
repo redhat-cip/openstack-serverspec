13cda95fe2250eb214a5bb4fadad4445266a28ed
Emilien Macchi <emilien.macchi@enovance.com>
2014-06-12 12:08:33 +0200

    neutron: update tests for provider network

--- spec/tests/networkcontroller/networkcontroller_spec.rb
+++ spec/tests/networkcontroller/networkcontroller_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 describe file('/etc/neutron/plugins/ml2/ml2_conf.ini') do
   its(:content) { should match /^tunnel_types ?= ?gre$/ }
-  its(:content) { should match /^type_drivers ?= ?gre,vlan$/ }
+  its(:content) { should match /^type_drivers ?= ?gre,vlan,flat$/ }
   its(:content) { should match /^network_vlan_ranges = ?physnet1:1000:2999$/ }
   its(:content) { should match /^tunnel_id_ranges ?= ?1:10000$/ }
   its(:content) { should match /^mechanism_drivers ?= ?openvswitch,l2population$/ }
@@ -13,7 +13,7 @@ describe file('/etc/neutron/plugins/ml2/ml2_conf.ini') do
   its(:content) { should match /^local_ip ?= ?#{property[:server_ip]}$/ }
   its(:content) { should match /^enable_tunneling ?= ?True$/ }
   its(:content) { should match /^integration_bridge ?= ?br-int$/ }
-  its(:content) { should match /^bridge_mappings ?= ?physnet1:br-eth1$/ }
+  its(:content) { should match /^bridge_mappings ?= ?public:br-pub$/ }
   its(:content) { should match /^firewall_driver ?= ?neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver$/ }
 end
 
