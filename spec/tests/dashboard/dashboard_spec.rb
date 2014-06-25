59d61d81ebfbd40b96c7b6b11ff4056f6eaa6c2b
Emilien Macchi <emilien.macchi@enovance.com>
2014-06-26 01:19:00 +0200

    dashboard: use the VIP to test the dashboard
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- spec/tests/dashboard/dashboard_spec.rb
+++ spec/tests/dashboard/dashboard_spec.rb
@@ -4,6 +4,6 @@ require 'spec_helper'
 # cloud::dashboard
 #
 
-describe command("curl #{property[:endpoint_proto]}://#{property[:server_ip]} | grep 301") do
+describe command("curl -k #{property[:endpoint_proto]}://#{property[:vip_public]} | grep 301") do
   it { should return_exit_status 0 }
 end
