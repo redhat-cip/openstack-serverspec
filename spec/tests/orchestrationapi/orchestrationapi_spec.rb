085302193c8d46ed6893a29d59704abab2e6b17f
Emilien Macchi <emilien.macchi@enovance.com>
2014-06-25 11:38:34 +0200

    heat: delete CLI test
    Because when it's installed in cluster, we need to crm cleanup the
    resoruce before. It should not block the deployment.
    
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- spec/tests/orchestrationapi/orchestrationapi_spec.rb
+++ spec/tests/orchestrationapi/orchestrationapi_spec.rb
@@ -11,7 +11,3 @@ end
 describe port(8003) do
   it { should be_listening.with('tcp') }
 end
-
-describe command("heat --os-username #{property[:ks_user_name]} --os-password #{property[:ks_user_password]} --os-tenant-name #{property[:ks_tenant_name]} --os-auth-url #{property[:endpoint_proto]}://#{property[:vip_public]}:5000/v2.0 stack-list") do
-  it { should return_exit_status 0 }
-end
