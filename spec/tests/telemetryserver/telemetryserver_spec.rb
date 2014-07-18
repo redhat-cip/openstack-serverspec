58c9dc61b81d96ef9ecb1cda2565a59d9b42ebbb
Dan <bodepd@gmail.com>
2014-07-17 19:09:42 -0700

    Make mongo installation optional for ceilometer
    introduces property ceilometer_backend

--- spec/tests/telemetryserver/telemetryserver_spec.rb
+++ spec/tests/telemetryserver/telemetryserver_spec.rb
@@ -8,8 +8,10 @@ describe port(8777) do
   it { should be_listening.with('tcp') }
 end
 
-describe port(27017) do
-  it { should be_listening.with('tcp') }
+if property[:ceilometer_backend] == 'mongodb'
+  describe port(27017) do
+    it { should be_listening.with('tcp') }
+  end
 end
 
 describe user('ceilometer') do
@@ -35,8 +37,10 @@ describe file('/etc/ceilometer/ceilometer.conf') do
   it { should be_grouped_into 'ceilometer' }
   its(:content) { should match /^rpc_backend=ceilometer.openstack.common.rpc.impl_kombu$/ }
   its(:content) { should match /^notification_topics=notifications$/ }
-  its(:content) { should match /^use_syslog=True$/ }
-  its(:content) { should match /^syslog_log_facility=LOG_LOCAL0$/ }
+  if property[:use_syslog]
+    its(:content) { should match /^use_syslog=True$/ }
+    its(:content) { should match /^syslog_log_facility=LOG_LOCAL0$/ }
+  end
 end
 
 describe command("ceilometer --os-username #{property[:ks_user_name]} --os-password #{property[:ks_user_password]} --os-tenant-name #{property[:ks_tenant_name]} --os-auth-url #{property[:endpoint_proto]}://#{property[:vip_public]}:5000/v2.0 meter-list") do
