188834898b4113374063565b61453236f208c019
Dan <bodepd@gmail.com>
2014-07-17 19:16:53 -0700

    Adds cinder_ceilometer property
    So that relevant checks to notification_driver
    driver are only made when cinder is configured to
    send data to ceilometer.

--- spec/tests/volumecontroller/volumecontroller_spec.rb
+++ spec/tests/volumecontroller/volumecontroller_spec.rb
@@ -39,8 +39,10 @@ end
 # cloud::volume
 #
 
-describe file('/etc/cinder/cinder.conf') do
-  it { should contain "notification_driver=cinder.openstack.common.notifier.rpc_notifier" }
+if property[:cinder_ceilometer]
+  describe file('/etc/cinder/cinder.conf') do
+    it { should contain "notification_driver=cinder.openstack.common.notifier.rpc_notifier" }
+  end
 end
 
 describe command("cinder --os-username #{property[:ks_user_name]} --os-password #{property[:ks_user_password]} --os-tenant-name #{property[:ks_tenant_name]} --os-auth-url #{property[:endpoint_proto]}://#{property[:vip_public]}:5000/v2.0 list") do
