bef0b7a8dc62b96d4410c1b25373469e8ab51b29
Emilien Macchi <emilien.macchi@enovance.com>
2014-09-11 14:27:08 -0400

    base: add a test for DNS
    Test DNS service by running nslookup against the public VIP.
    
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- spec/tests/base/base_spec.rb
+++ spec/tests/base/base_spec.rb
@@ -7,3 +7,8 @@ require 'spec_helper'
 describe port(22) do
   it { should be_listening.with('tcp') }
 end
+
+# test if DNS is working
+describe command("nslookup #{property[:vip_public]}") do
+  it { should return_exit_status 0 }
+end
