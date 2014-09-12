7cea23c57106585354b1e7a824493116aa4daa52
Emilien Macchi <emilien.macchi@enovance.com>
2014-09-12 14:32:16 -0400

    tests: timeout DNS to 1 sec max
    If a DNS request takes more than 1 second, this is not acceptable, then
    the test have to fail.
    
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- spec/tests/base/base_spec.rb
+++ spec/tests/base/base_spec.rb
@@ -9,7 +9,7 @@ describe port(22) do
 end
 
 # test if DNS is working
-describe command("dig #{property[:vip_internal_fqdn]}") do
+describe command("timeout 1 dig #{property[:vip_internal_fqdn]}") do
   it { should return_exit_status 0 }
 end
 
