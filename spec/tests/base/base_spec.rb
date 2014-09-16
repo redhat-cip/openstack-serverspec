67e27f1f0ea54f52058d3850aef89af69c26e422
Emilien Macchi <emilien.macchi@enovance.com>
2014-09-15 21:41:03 -0400

    NTP functionnal tests: move + note
    - Move the NTP tests to spof class, since NTP is required to build the
      Pacemaker cluster.
    - Add a note which says that the package is not yet in Debian:
      https://ftp-master.debian.org/new/ntpstat_0.0.0.1-1.html
    
    Signed-off-by: Emilien Macchi <emilien.macchi@enovance.com>

--- spec/tests/base/base_spec.rb
+++ spec/tests/base/base_spec.rb
@@ -12,12 +12,3 @@ end
 describe command("timeout 1 dig #{property[:vip_internal_fqdn]}") do
   it { should return_exit_status 0 }
 end
-
-# test if NTP is working
-# using 'ntpstat' command
-# exit status 0 - Clock is synchronised.
-# exit status 1 - Clock is not synchronised.
-# exit status 2 - If clock state is indeterminant, for example if ntpd is not contactable
-describe command("ntpstat") do
-  it { should return_exit_status 0 }
-end
--- spec/tests/spof/spof_spec.rb
+++ spec/tests/spof/spof_spec.rb
@@ -7,3 +7,14 @@ require 'spec_helper'
 describe command("crm_mon -1 | grep ceilometer-agent-central") do
   it { should return_exit_status 0 }
 end
+
+# NTP is needed to synchronize the cluster
+# using 'ntpstat' command
+# Note: ntpstat is not yet present in Debian official repositories.
+# https://ftp-master.debian.org/new/ntpstat_0.0.0.1-1.html
+# exit status 0 - Clock is synchronised.
+# exit status 1 - Clock is not synchronised.
+# exit status 2 - If clock state is indeterminant, for example if ntpd is not contactable
+describe command("ntpstat") do
+  it { should return_exit_status 0 }
+end
