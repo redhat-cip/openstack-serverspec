6834dfb7614e00d88d5e43da2d385e95540db53d
Emilien Macchi <emilien.macchi@enovance.com>
2014-09-26 15:01:21 -0400

    networkcontroller: drop ml2 config check
    we don't want to be stuck on ml2 plugin and may want a better
    flexibility when deploying OpenStack.

--- spec/tests/networkcontroller/networkcontroller_spec.rb
+++ spec/tests/networkcontroller/networkcontroller_spec.rb
@@ -2,21 +2,6 @@ require 'spec_helper'
 
 # Neutron Server
 
-describe file('/etc/neutron/plugins/ml2/ml2_conf.ini') do
-  its(:content) { should match /^tunnel_types ?= ?gre$/ }
-  its(:content) { should match /^type_drivers ?= ?gre,vlan,flat$/ }
-  its(:content) { should match /^network_vlan_ranges = ?physnet1:1000:2999$/ }
-  its(:content) { should match /^tunnel_id_ranges ?= ?1:10000$/ }
-  its(:content) { should match /^mechanism_drivers ?= ?openvswitch,l2population$/ }
-  its(:content) { should match /^l2_population ?= ?True$/ }
-  its(:content) { should match /^polling_interval ?= ?15$/ }
-  its(:content) { should match /^local_ip ?= ?#{property[:server_ip]}$/ }
-  its(:content) { should match /^enable_tunneling ?= ?True$/ }
-  its(:content) { should match /^integration_bridge ?= ?br-int$/ }
-  its(:content) { should match /^bridge_mappings ?= ?public:br-pub$/ }
-  its(:content) { should match /^firewall_driver ?= ?neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver$/ }
-end
-
 describe file('/etc/neutron/neutron.conf') do
   its(:content) { should match /^nova_admin_username ?= ?nova$/ }
 end
