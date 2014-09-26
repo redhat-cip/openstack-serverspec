8a72087aae55d8b53e38e8214a870d119b3bed38
Emilien Macchi <emilien.macchi@enovance.com>
2014-09-26 15:05:56 -0400

    controller: don't stick on VIF driver configuration
    Depending of VIF technology, nova config should not be hardcoded.
    If we need later more driver testing, we may add a new parameter but
    now, let's delete the flag check.

--- spec/tests/controller/controller_spec.rb
+++ spec/tests/controller/controller_spec.rb
@@ -22,7 +22,6 @@ end
 
 describe file('/etc/nova/nova.conf') do
   it { should contain "servicegroup_driver=mc" }
-  it { should contain "vif_driver=nova.virt.libvirt.vif.LibvirtGenericVIFDriver" }
 end
 
 describe port(6082) do
