e545ef5452c184584d1c98f9b1a6d5a77c8502f0
Yanis Guenane <yguenane@gmail.com>
2014-12-05 09:52:48 -0500

    Deprecate endpoint_proto
    Deprecate endpoint_proto and move to use endpoint_protocol instead

--- spec/tests/identity/identity_spec.rb
+++ spec/tests/identity/identity_spec.rb
@@ -11,6 +11,6 @@ describe port(35357) do
   it { should be_listening.with('tcp') }
 end
 
-describe command("keystone --os-username #{property[:ks_user_name]} --os-password #{property[:ks_user_password]} --os-tenant-name #{property[:ks_tenant_name]} --os-auth-url #{property[:endpoint_proto]}://#{property[:vip_public]}:5000/v2.0 user-list") do
+describe command("keystone --os-username #{property[:ks_user_name]} --os-password #{property[:ks_user_password]} --os-tenant-name #{property[:ks_tenant_name]} --os-auth-url #{property[:endpoint_protocol]}://#{property[:vip_public]}:5000/v2.0 user-list") do
   it { should return_exit_status 0 }
 end
