8d2856a922c87499538ed96b2cff50e1a03ee37f
Yanis Guenane <yanis.guenane@enovance.com>
2015-03-15 12:16:49 +0100

    polling agent: Remove test about spof class
    Since the cloud::spof class has been removed we remove the test
    that goes with it.

--- spec/tests/base/base_spec.rb
+++ spec/tests/base/base_spec.rb
@@ -12,3 +12,14 @@ end
 describe command("timeout 1 dig #{property[:vip_public]}") do
   it { should return_exit_status 0 }
 end
+
+# NTP is needed to synchronize the cluster
+#  using 'ntpstat' command
+#  Note: ntpstat is not yet present in Debian official repositories.
+#  https://ftp-master.debian.org/new/ntpstat_0.0.0.1-1.html
+#  exit status 0 - Clock is synchronised.
+#  exit status 1 - Clock is not synchronised.
+#  exit status 2 - If clock state is indeterminant, for example if ntpd is not contactable
+describe command("ntpstat") do
+  it { should return_exit_status 0 }
+end
