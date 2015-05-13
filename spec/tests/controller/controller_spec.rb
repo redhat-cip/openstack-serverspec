57aab75badde0bc2a2c9e785a0d304f7fc020970
Emilien Macchi <emilien@redhat.com>
2015-05-13 13:15:37 -0400

    controller: also test nova list
    We already test nova flavor-list but testing nova list also tries some
    DB access to instances tables, that can cause troubles lately.
    
    We may want to test it during the deployment and fail if the command
    does not work.

--- spec/tests/controller/controller_spec.rb
+++ spec/tests/controller/controller_spec.rb
@@ -8,6 +8,10 @@ describe command("timeout 10 nova --os-username #{property[:ks_user_name]} --os-
   it { should return_exit_status 0 }
 end
 
+describe command("timeout 10 nova --os-username #{property[:ks_user_name]} --os-password #{property[:ks_user_password]} --os-tenant-name #{property[:ks_tenant_name]} --os-auth-url #{property[:endpoint_protocol]}://#{property[:vip_public]}:5000/v2.0 list") do
+  it { should return_exit_status 0 }
+end
+
 describe port(8773) do
   it { should be_listening.with('tcp') }
 end
